095839a686f93236f23a8989ee58424a41540c8b
Wu Zhen <wuzhen0808@gmail.com>
2013-03-26 19:14:58 +0800

    Signed-off-by: Wu Zhen <wuzhen0808@gmail.com>

---
+++ fs-uiclient/src/api/java/com/fs/uiclient/api/gwt/client/facebook/FbLoginButtonJSO.java
@@ -0,0 +1,19 @@
+/**
+ *  
+ */
+package com.fs.uiclient.api.gwt.client.facebook;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+/**
+ * @author wu
+ * 
+ */
+public final class FbLoginButtonJSO extends JavaScriptObject {
+
+	protected FbLoginButtonJSO() {
+
+	}
+	
+	
+}
